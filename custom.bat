2591e16b97ebd522e550332ec9d65e6dd25b61ce
Victor <gamehostplay@gmail.com>
2016-04-09 17:40:34 +0500

    fix

--- custom.bat
+++ custom.bat
@@ -9,6 +9,7 @@ for /d %%I in ("*.zip.bzprj") do (
 popd
 set ROM=%CD%\%RD%\baseROM
 set Tools=%CD%\data\tools
+set pht=%CD%\data\tools\ph-tools
 set Repo=%CD%\repositories\patches\main\boot
 
 del %Tools%\*.img
@@ -20,7 +21,7 @@ move /Y %ROM%\boot_PORT.img %Tools%\boot_PORT.img
 
 call %Tools%\MTK_unpack.bat boot_PORT.img 
 
-java -jar %Tools%\CameraReplacer.jar
+java -jar %pht%\ph-cr.jar
 
 copy /Y %Repo%\kernel\* %Tools%\boot_PORT\
 copy /Y %Repo%\rmdisk\* %Tools%\boot_PORT\rmdisk\
