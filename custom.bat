b8832a29272465d871eef59c3deab219e2ec01a6
Max Pustovalov <mxp.dev@yandex.ru>
2016-05-15 16:45:01 +0900

    modify custom.bat for marp

--- custom.bat
+++ custom.bat
@@ -10,7 +10,7 @@ for /d %%I in ("*.zip.bzprj") do (set RD=%%~I)
 
 set ROM=%CD%\%RD%\baseROM
 set Tools=%CD%\data\tools
-set pht=%CD%\data\tools\ph-tools
+set marpt=%CD%\data\tools\marp-tools
 set Repo=%CD%\repositories\patches\main\boot
 
 echo clean dirs...
@@ -26,9 +26,9 @@ move /Y %ROM%\boot_PORT.img %Tools%\boot_PORT.img
 
 call %Tools%\MTK_unpack.bat boot_PORT.img 
 
-java -jar %pht%\ph-cr.jar
-java -jar %pht%\ph-id.jar
-java -jar %pht%\ph-us.jar %ROM%
+python %marpt%\marp-cr.py
+python -jar %marpt%\marp-id.py
+python -jar %marpt%\marp-us.py %ROM%
 
 copy /Y %Repo%\kernel\* %Tools%\boot_PORT\
 copy /Y %Repo%\rmdisk\* %Tools%\boot_PORT\rmdisk\
