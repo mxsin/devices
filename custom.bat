eaa047280a84ae21bee3be7ae3426371ff27b05c
Max Pustovalov <mxp.dev@yandex.ru>
2016-05-18 17:32:49 +0900

    fixed bug

--- custom.bat
+++ custom.bat
@@ -1,24 +1,23 @@
 @echo off
 
-echo Original custom.bat by Victor Malyshev (@I1PABIJJA) i1pabijja@gmail.com
-echo Current custom.bat by Max Pustovalov (@mxsin) sir.mxp@yandex.ru
+echo custom.bat by Max Pustovalov (@mxsin) sir.mxp@yandex.ru
 
-echo init folders...
+echo initialization...
 
 set CD=%~dp0
-for /d %%I in ("*.zip.bzprj") do (set RD=%%~I)
+for /d %I in ("*.zip.bzprj") do (set RD=%~I)
 
 set ROM=%CD%\%RD%\baseROM
 set Tools=%CD%\data\tools
 set marpt=%CD%\data\tools\marp_tools
 set Repo=%CD%\repositories\patches\main\boot
 
-echo clean dirs...
+echo cleaning tools directory...
 
 del %Tools%\*.img
 rmdir /S /Q %Tools%\boot_PORT
 
-echo Repacking boot.img...
+echo unpacking boot...
 
 ren %ROM%\boot.img boot_PORT.img
 
@@ -26,21 +25,26 @@ move /Y %ROM%\boot_PORT.img %Tools%\boot_PORT.img
 
 call %Tools%\MTK_unpack.bat boot_PORT.img > nul
 
-cd /d %CD%
+echo modifying updater-script...
 
 rem python %marpt%\marp_custom_boot_data.py
 rem python %marpt%\marp_initd_support.py
 call %marpt%\marp_updater_script.py %ROM% mxsin MultiROM
-pause
+
+echo replacing necessary files...
 
 copy /Y %Repo%\kernel\* %Tools%\boot_PORT\
 copy /Y %Repo%\rmdisk\* %Tools%\boot_PORT\rmdisk\
+
+echo packing boot...
 
 call %Tools%\MTK_pack.bat boot_PORT
 
 move /Y %Tools%\new_image.img %ROM%\boot.img
 
+echo post-process cleaning...
+
 del %Tools%\*.img
 rmdir /S /Q %Tools%\boot_PORT
 
-echo Repacking Boot.img finished...
+echo work done!
